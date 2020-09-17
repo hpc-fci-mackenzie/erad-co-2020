262ab76faa7be3288923d5ec7c126bc01c62e63d
Calebe de Paula Bianchini <prof.calebe.bianchini@gmail.com>
2020-09-16 21:39:43 -0300

    Adicionando resposta do 4o desafio

--- desafios/desafio4.c
+++ desafios/desafio4.c
@@ -3,16 +3,38 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSOS (1<<30)
+#define PASSOS (1<<10)
 
 int main()
 {
     double x, pi, soma = 0.0, passo;
     passo = 1.0 / (double)PASSOS;
-    for (int i = 0; i < PASSOS; i++) {
+    double *somas;
+    somas = (double*)malloc(sizeof(double)*omp_get_max_threads());
+    if(somas == NULL) {
+        printf("Erro alocando memoria\n");
+        return 1;
+    }
+#pragma omp parallel
+{
+    int id = omp_get_thread_num();
+    int slot = PASSOS / omp_get_num_threads();
+    int inicio = id * slot;
+    int fim = inicio + slot;
+    if(id == omp_get_num_threads()-1) {
+        fim = PASSOS;
+    }
+    double soma_local = 0.0;
+    for (int i = inicio; i < fim; i++) {
         x = (i + 0.5) * passo;
-        soma += 4.0 / (1.0 + x * x);
+        soma_local += 4.0 / (1.0 + x * x);
+    }
+    somas[id] = soma_local;
+}
+    for(int i=0; i<omp_get_max_threads(); i++) {
+        soma += somas[i];
     }
     pi = passo * soma;
     printf("PI:%.15lf\n", pi);
+    free(somas);
 }
